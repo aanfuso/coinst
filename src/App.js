c4fcaae5db7c1dcae3cff7d67578e611dd0339a1
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-06 19:53:50 +0400

    Apply layout to the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Container } from "@mui/material";
+
+import Layout from "./lib/components/Layout";
+
+import { base } from 'lib/themes';
+
+const FOOTER_PROPS = {
+  topText: 'Made with ❤️ by',
+  bottomText: 'Agustin Anfuso',
+  url: 'https://anfu.space',
+};
+
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Layout
+      theme={base}
+      footerProps={FOOTER_PROPS}
+    >
+      <Container sx={{ height: '65vh', mt: 10 }}>
+      </Container>
+    </Layout>
   );
 }
 
