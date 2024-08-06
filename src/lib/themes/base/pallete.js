e73edbc20347c2b5a09e1e8efd71a7665835167f
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-06 19:46:15 +0400

    Add base UI theme

---
+++ src/lib/themes/base/pallete.js
@@ -0,0 +1,17 @@
+const primary = {
+  main: 'hsl(275, 100%, 26%)',
+};
+
+const secondary = {
+  main: 'hsl(256, 49%, 67%)',
+};
+
+const accent = {
+  main: 'hsl(208, 18%, 16%)',
+};
+
+export const palette = {
+  primary,
+  secondary,
+  accent,
+};
---
+++ src/lib/themes/base/typography.js
@@ -0,0 +1,34 @@
+const {
+  fontFamily,
+  fontSize,
+  htmlFontSize,
+  light,
+  regular,
+  medium,
+  semiBold,
+  bold,
+  textTransform,
+} = {
+  fontFamily: '"Source Sans 3", sans-serif',
+  fontSize: 16,
+  bold: 700,
+};
+
+const fontVariants = {
+  body2: {
+    fontSize: 14,
+  },
+};
+
+export const typography = {
+  fontFamily,
+  fontSize,
+  htmlFontSize,
+  fontWeightLight: light,
+  fontWeightRegular: regular,
+  fontWeightMedium: medium,
+  fontWeightSemiBold: semiBold,
+  fontWeightBold: bold,
+  textTransform,
+  ...fontVariants,
+};
