fe295c432223567fa9a63c2c54f6854f45572d74
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-08 00:09:50 +0400

    Add PriceChart loading state and color

--- src/widgets/PriceChart/index.js
+++ src/widgets/PriceChart/index.js
@@ -8,16 +8,22 @@ const KEY_TO_LABEL = {
 export default function ChartPrice({ updates }) {
   return (
     <LineChart
+      loading={updates.length === 0}
       xAxis={[
         {
           dataKey: 'time',
           valueFormatter: (value) => new Date(value).toLocaleTimeString(),
         },
       ]}
+      slotProps={{
+        loadingOverlay: { message: 'Data should be available soon.' },
+        noDataOverlay: { message: 'Select some data to display.' },
+      }}
       series={Object.keys(KEY_TO_LABEL).map((key) => ({
         dataKey: key,
         label: KEY_TO_LABEL[key],
         showMark: false,
+        color: '#EDC949',
       }))}
       dataset={updates}
       grid={{ vertical: true, horizontal: true }}
