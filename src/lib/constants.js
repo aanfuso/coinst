0f52e6cf513b1b3eedf28f4143493cf3af95a099
Agustin Anfuso <anfusoagustin89@gmail.com>
2024-08-07 15:42:13 +0400

    Add constants file

---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+const BTC_USD = 'BTC-USD';
+const ETH_USD = 'ETH-USD';
+const LTC_USD = 'LTC-USD';
+const BCH_USD = 'BCH-USD';
+
+export const LISTED_PRODUCTS = [BTC_USD, ETH_USD, LTC_USD, BCH_USD];
